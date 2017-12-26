076bf09a4786c50afdb10ab70c5fd76f36299879
Carter Chavez <Carter.chavez17@gmail.com>
2017-12-25 16:47:57 -0800

    init project and create header

---
+++ App.js
@@ -0,0 +1,38 @@
+import React, { Component } from 'react';
+import {
+  Platform,
+  StyleSheet,
+  Text,
+  View
+} from 'react-native';
+import Header from './src/components/Header'
+
+
+export default class App extends Component<{}> {
+  render() {
+    return (
+      <View>
+        <Header />
+      </View>
+    );
+  }
+}
+
+// const styles = StyleSheet.create({
+//   container: {
+//     flex: 1,
+//     justifyContent: 'center',
+//     alignItems: 'center',
+//     backgroundColor: '#F5FCFF',
+//   },
+//   welcome: {
+//     fontSize: 20,
+//     textAlign: 'center',
+//     margin: 10,
+//   },
+//   instructions: {
+//     textAlign: 'center',
+//     color: '#333333',
+//     marginBottom: 5,
+//   },
+// });
---
+++ src/components/Header.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import { Text, View } from 'react-native';
+
+
+const Header = () => {
+	const { textStyle, viewStyle } = styles;
+	return (
+		<View style={viewStyle}>
+			<Text style={textStyle}>Top Currencies</Text>
+		</View>
+	)
+}
+
+const styles = {
+	viewStyle: {
+		justifyContent: 'center',
+		alignItems: 'center',
+		backgroundColor: '#F8F8F8',
+		height: 60,
+		paddingTop: 15,
+		shadowColor: '#000',
+		shadowOffset: { width: 0, height: 2 },
+		shadowOpacity: 0.1,
+		elevation: 2,
+		position: 'relative'
+	},
+	textStyle: {
+		fontSize: 30,
+	}
+}
+
+export default Header;
